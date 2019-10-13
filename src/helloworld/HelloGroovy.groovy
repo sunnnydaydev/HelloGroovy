634f703ef9c4a4b539db2384d43682a6bc8a9db5
Administrator <1246771571@qq.com>
2019-10-13 16:25:46 +0800

    change dir

--- src/helloworld/HelloGroovy.groovy
+++ src/helloworld/HelloGroovy.groovy
@@ -1,7 +1,8 @@
+package helloworld
 /**
  * Created by sunnyDay on 2019/10/8 17:58
  */
-//class HelloGroovy {
+//class helloworld.HelloGroovy {
 //     public static void main(String[] args) {
 //        System.out.println("hello groovy")
 //    }
